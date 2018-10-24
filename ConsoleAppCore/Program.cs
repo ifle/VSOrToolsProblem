c531eea7808023a2e145fff8ef92f50d44116c0b
ifle <igor609@gmail.com>
2018-10-24 17:52:52 +0300

    Init

---
+++ ConsoleAppCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrToolsVRP.Solve();
+            Console.ReadKey();
+        }
+    }
+}
